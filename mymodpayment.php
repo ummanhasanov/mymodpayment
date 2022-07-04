b26e3ac386ea41952dccba4d83b958879646ec7d
Umman Hasanov <umman3018@gmail.com>
2022-07-04 18:24:39 +0400

    displayPayment

---
+++ mymodpayment.php
@@ -0,0 +1,42 @@
+<?php
+
+class MyModPayment extends PaymentModule {
+
+    public function __construct() {
+        $this->name = 'mymodpayment';
+        $this->tab = 'payment_gateways';
+        $this->version = '0.1';
+        $this->author = 'Umman Hasanov';
+        $this->bootstrap = true;
+        parent::__construct();
+        $this->displayName = $this->l('MyMod payment');
+        $this->description = $this->l('A simple payment module');
+    }
+
+    public function install() {
+        if (!parent::install() ||
+                !$this->registerHook('displayPayment') ||
+                !$this->registerHook('displayPaymentReturn')) {
+            return false;
+        }
+        return true;
+    }
+
+    public function getHookController($hook_name) {
+        // Include the controller file
+        require_once (dirname(__FILE__) . 'controllers/hook' . $hook_name . '.php');
+
+        // Build dynamically the controller name
+        $controller_name = $this->name . $hook_name . 'Controller';
+
+        // Instantiate controller
+        $controller = new $controller_name($this, __FILE__, $this->_path);
+
+        return $controller;
+    }
+
+    public function hookDisplayPayment($params) {
+        return $this->display(__FILE__, 'displayPayment.tpl');
+    }
+
+}
---
+++ views/templates/hook/displayPayment.tpl
@@ -0,0 +1,9 @@
+
+<div class="row">
+    <div class="col-xs-12 col-md-6">
+        <p class="payment_module"><a href="#" class="mymodpayment">
+                {l s='Pay with simple MyMod payment module' mod='mymodpayment'}
+            </a></p>
+    </div>
+</div>
+            <h1><strong> S A L A M </strong></h1>
